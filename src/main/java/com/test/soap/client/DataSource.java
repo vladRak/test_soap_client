bd610029125fe31df34b8b9acc8350d9a73dafe4
vladRak <vladrakitianskiy@gmail.com>
2019-04-10 13:57:56 +0300

    snapshot 0.8.0

--- src/main/java/com/test/soap/client/DataSource.java
+++ src/main/java/com/test/soap/client/DataSource.java
@@ -6,7 +6,7 @@ import com.test.soap.client.exception.DataAccessException;
  * DataSource interface used for saving incoming data
  *
  * @author vladRak
- * @see ClientData,DataAccessException
+ * @see ClientData
  */
 public interface DataSource<T extends ClientData> {
 
@@ -15,7 +15,7 @@ public interface DataSource<T extends ClientData> {
      *
      * @throws DataAccessException If storing finish with out success exception occurred
      * @author vladRak
-     * @see ClientData
+     * @see ClientData,DataAccessException
      */
     void save(T data) throws DataAccessException;
 }
